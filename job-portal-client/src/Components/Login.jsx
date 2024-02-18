754f47bb60b8f31d8b7f23c74f0edc361458f24a
Ashwini Krishnaji Rao <ashwinikrishnajirao@gmail.com>
2024-02-18 19:49:20 +0530

    design

--- job-portal-client/src/Components/Login.jsx
+++ job-portal-client/src/Components/Login.jsx
@@ -24,6 +24,7 @@ createUserWithEmailAndPassword(auth, email, password)
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
+    alert("incorrect username or password");
   });
     }
 
@@ -43,38 +44,33 @@ createUserWithEmailAndPassword(auth, email, password)
     }
   return (
     <div>
-<form onSubmit={emailLogin}>
-        <div className="flex justify-start md:flex-row flex-col md:gap-0 gap-4">
-          <div className="flex md:round-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 
-          focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2  w-full">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter your email"
-              className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 
-              placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6'
-              />
-         
-          </div>
-          <div className="flex md:round-s-none rounded shadow-sm ring-1 ring-inset ring-gray-300 
-          focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3  w-full">
-            <input
-              type="password"
-              name="Password"
-              id="Password"
-              placeholder="Enter your Password"
-              className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 
-              placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6'
-              />
-          </div>
+<form onSubmit={emailLogin} className="flex flex-col items-center justify-center">
+  <div className="flex flex-col w-full gap-4 md:w-1/2">
+    <div className="flex rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Enter your email"
+        className="flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6"
+      />
+    </div>
+    <div className="flex rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+      <input
+        type="password"
+        name="Password"
+        id="Password"
+        placeholder="Enter your Password"
+        className="flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6"
+      />
+    </div>
+    <button type="submit" className="bg-blue py-2 px-8 text-white rounded">Login/Signup</button>
+  </div>
+</form>
 
-          <button type="submit" className="bg-blue py-2 px-8 text-white ">Login/Signup</button>
-        </div>
-      </form>
-    
-    <div className='h-screen w-full flex items-center justify-center'>
-        <button className='bg-blue px-8 py-2 text-white' onClick={handleLogin}>Login</button>
+   
+    <div className='h-screen w-full flex items-center justify-center'> 
+        <button className='bg-blue px-8 py-2 text-white' onClick={handleLogin}>Continue with Google</button>
     </div>
     </div>
   )
