49f87dc26bf3497dae8b1ccf9da838aa19b6dcbd
Ashwini Krishnaji Rao <ashwinikrishnajirao@gmail.com>
2024-03-09 03:02:49 +0530

    Color Fixed

--- job-portal-client/src/Pages/MyJobs.jsx
+++ job-portal-client/src/Pages/MyJobs.jsx
@@ -118,7 +118,7 @@ navigate("/login")
                 <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
                   <Link to="/post-job">
                     <button
-                      className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      className="bg-blue text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
                     >
                       Post A New Job
