013ce93478dc60d38c36c35fbba48ce0cd35a4e8
Ashwini Krishnaji Rao <ashwinikrishnajirao@gmail.com>
2024-03-09 02:13:11 +0530

    Application Form

--- job-portal-client/src/Pages/JobDetails.jsx
+++ job-portal-client/src/Pages/JobDetails.jsx
@@ -1,35 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Swal from 'sweetalert2'
-import PageHeader from '../Components/PageHeader';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import PageHeader from "../Components/PageHeader";
 
 const JobDetails = () => {
-    const {id} = useParams();
-    const [job, setJob] =useState([])
-    useEffect(()=>{
-        fetch(`http://localhost:5000/all-jobs/${id}`).then(res => res.json()).then(data => setJob(data))
-    },[])
+  const { id } = useParams();
+  const [job, setJob] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/all-jobs/${id}`)
+      .then((res) => res.json())
+      .then((data) => setJob(data));
+  }, []);
 
-const handleApply = async() => {
-    const { value: url } = await Swal.fire({
-        input: "url",
-        inputLabel: "URL address",
-        inputPlaceholder: "Enter the URL"
-      });
-      if (url) {
-        Swal.fire(`Entered URL: ${url}`);
-      }
-}
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file.type === "application/pdf") {
+      setPdfFile(file);
+      Swal.fire(`Entered PDF: ${file.name}`);
+    } else {
+      alert("Please upload a PDF file.");
+    }
+  };
+
+  const handleSubmit = () => {
+    Swal.fire("Your application has been submitted.");
+    setPdfFile(null);
+  };
 
   return (
-    <div className='max-w-screen-2xl container mx-auto xl:px-24 l:px-24 px-4'>
-        <PageHeader title={"Single Job Page"} path={"single job"}/>
+    <div>
+      <div className="max-w-screen-2xl container mx-auto xl:px-24 l:px-24 px-4">
+        <PageHeader title={"Single Job Page"} path={"single job"} />
         <h2>JobDetails: {id}</h2>
         <h1>{job.jobTitle}</h1>
-        <button className='bg-blue px-8 py-2 text-white' onClick={handleApply}>Apply Now</button>
-        </div>
-    
-  )
-}
+      </div>
+      <div className="max-w-screen-2xl mt-4 mx-auto xl:px-24 l:px-24 px-4">
+        <h1 className="text-blue text-lg">Upload Your Resume To Apply</h1>
+        <input type="file" accept=".pdf" onChange={handleFileChange} />
 
-export default JobDetails
+        {pdfFile && (
+          <div>
+            <p>Selected PDF: {pdfFile.name}</p>
+            <button
+              className="bg-blue px-8 py-2 text-white"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default JobDetails;
